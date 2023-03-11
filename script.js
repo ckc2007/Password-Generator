4b6849103d3cd72a010e9567d30ff5ee9d3ea520
ckc2007 <ckc2007@gmail.com>
2023-03-11 15:18:17 -0500

    commented out all console logs

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function generatePassword() {
     "How many characters would you like this password to be? (please enter a number between 8 and 128)"
   );
   // debug console log
-  console.log(passLen);
+  // console.log(passLen);
 
   // determine if user input is valid for password length
   function passLenCheck(x) {
@@ -33,7 +33,7 @@ function generatePassword() {
         `awesome, the password length meets the criteria! a password with ${x} characters will be generated.`
       );
       // debug console log
-      console.log(x);
+      // console.log(x);
       // return the confirmned password length value
       return x;
       // if the password is not in range do this:
@@ -42,7 +42,7 @@ function generatePassword() {
         "I'm sorry, that was an invalid entry. How many characters would you like this password to be? (please enter a number between 8 and 128.)"
       );
       // debug console log
-      console.log(x);
+      // console.log(x);
       // must have a return statement here << this was the bug
       return passLenCheck(x);
     }
@@ -60,7 +60,7 @@ function generatePassword() {
       "Quesetion 1 of 4: Do you want to use lowercase characters?"
     );
     // debug console log
-    console.log(lowerCase);
+    // console.log(lowerCase);
     // user message so they know what they selected.
     if (lowerCase) {
       window.alert("Ok, your password will include lowercase characters!");
@@ -73,7 +73,7 @@ function generatePassword() {
       "Question 2 of 4: Do you want to use uppercase characters?"
     );
     // debug console log
-    console.log(upperCase);
+    // console.log(upperCase);
     // user message so they know what they selected.
     if (upperCase) {
       window.alert("Ok, your password will include uppercase characters!");
@@ -86,7 +86,7 @@ function generatePassword() {
       "Question 3 of 4: Do you want to use numeric characters?"
     );
     // debug console log
-    console.log(numeric);
+    // console.log(numeric);
     // user message so they know what they selected.
     if (numeric) {
       window.alert("Ok, your password will include numeric characters!");
@@ -97,7 +97,7 @@ function generatePassword() {
     // confirm special characters?
     special = confirm("Question 4 of4: Do you want to use special characters?");
     // debug console log
-    console.log(special);
+    // console.log(special);
     // if the user has not chosen at least one option:
     if (!upperCase && !lowerCase && !numeric && !special) {
       window.alert(
@@ -113,7 +113,7 @@ function generatePassword() {
 
   // call the charChecker function:
   charChecker();
-  console.log(confirmedPassLen + "alert");
+  // console.log(confirmedPassLen + "alert");
   // the bug is with this variable above
 
   // make a pool (array) based on the selection criteria:
@@ -182,7 +182,7 @@ function generatePassword() {
   }
 
   // debug console log
-  console.log(poolArr);
+  // console.log(poolArr);
 
   // need a random number generator function that will return a random number between two numbers (range [a,b]):
   function randomIndexFromRange(a, b) {
@@ -201,7 +201,7 @@ function generatePassword() {
   if (lowerCase) {
     randomIndex = randomIndexFromRange(0, lowerCaseArr.length - 1);
     // debug console log
-    console.log(randomIndex);
+    // console.log(randomIndex);
     // add it to our solution array
     solution.push(lowerCaseArr[randomIndex]);
     // take one away from our count
@@ -209,25 +209,25 @@ function generatePassword() {
   }
   if (upperCase) {
     randomIndex = randomIndexFromRange(0, upperCaseArr.length - 1);
-    console.log(randomIndex);
+    // console.log(randomIndex);
     solution.push(upperCaseArr[randomIndex]);
     count--;
   }
   if (numeric) {
     randomIndex = randomIndexFromRange(0, numericArr.length - 1);
-    console.log(randomIndex);
+    // console.log(randomIndex);
     solution.push(numericArr[randomIndex]);
     count--;
   }
   if (special) {
     randomIndex = randomIndexFromRange(0, specialArr.length - 1);
-    console.log(randomIndex);
+    // console.log(randomIndex);
     solution.push(specialArr[randomIndex]);
     count--;
   }
 
   // debug console log - making sure we have one of each...
-  console.log(solution);
+  // console.log(solution);
 
   // for all the remaining characters, create a new random index and fill up the solution array:
   for (let i = 0; i < count; i++) {
@@ -236,11 +236,11 @@ function generatePassword() {
   }
 
   // debug console log to make sure our solution holds the required number of characters:
-  console.log(solution);
+  // console.log(solution);
   // we need to return a string, so join the array and .toString() to make sure all characters are a single string:
   let solutionString = solution.join("").toString();
   // debug console log to make sure we have a string
-  console.log(solutionString);
+  // console.log(solutionString);
   // the final output is the string of random characters of the desired length
   return solutionString;
 
